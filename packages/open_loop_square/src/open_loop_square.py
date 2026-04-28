fb139854d3496a00bb35d4776504de655cafa81a
obiwankenobi1935 <kavishchoudhary1935@gmail.com>
2026-04-28 09:40:54 +0530

    clean square motion code

--- packages/open_loop_square/src/open_loop_square.py
+++ packages/open_loop_square/src/open_loop_square.py
@@ -40,7 +40,7 @@ class Drive_Square:
 
             self.cmd_msg.header.stamp = rospy.Time.now()
             self.cmd_msg.v = 0.0
-            self.cmd_msg.omega = 2.8
+            self.cmd_msg.omega = 2.87
             self.pub.publish(self.cmd_msg)
             rospy.loginfo("Turning!")
             rospy.sleep(0.5)
